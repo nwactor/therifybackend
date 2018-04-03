189227e41f7a3342903c1fd95cac869dc6249b74
nwactor <nickwactor@gmail.com>
2018-04-02 19:16:48 -0700

    modern model and controller updates

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,21 +7,31 @@ module.exports = {
 	// 		.catch(err => res.status(422).json(err));
 	// },
 	login: function(req, res) {
+		console.log(req.body);
 		db.User
-			.insertOne(req.body)
+			.findOne({'email': req.body.email})
 			.then(result => {
-				if(result.acknowledged) { //the user was new
-					db.User.findById(result.id)
+				if(result === null) { //the user was new
+					console.log("New user registering.");
+					db.User
+						.create(req.body)
 						.then(newUser => res.json(newUser))
 						.catch(err => res.status(422).json(err));
-				} else { //find and return the user
-					db.User.findOne({email: req.body.email})
-						.then(foundUser => res.json(foundUser))
-						.catch(err => res.status(422).json(err));
+				} else {
+					console.log("Existing user returning");
+					res.json(result);
 				}
-			}).catch(err => res.status(422).json(err));
+			})
+			.catch(err => res.status(422).json(err));
 	},
 	getPhotos: function(req, res) {
-
+		//the request will have the user's email, so find the user with that email
+		db.User.findOne({email: req.body.email})
+			.then(user => {
+				db.Photos.find({user: user.id})
+					.then(userPhotos => {
+						res.json(userPhotos);
+					}).catch(err => console.log(err));
+			})
 	}
 };
--- server.js
+++ server.js
@@ -9,7 +9,8 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json({limit: '10mb'})); //for some reason, this is needed to see the body from fetch requests
 
 var routes = require("./routes/routes");
 app.use("/", routes);
@@ -19,9 +20,9 @@ mongoose.Promise = Promise; //Set Mongo to use promises for asynch queries
 //connect to db
 mongoose.connect("mongodb://localhost/therify", {
   //useMongoClient: true //got a warning saying to remove this
-});
+}).then(response => console.log("Connected to server."));
 
 //start server
-app.listen(PORT, '0.0.0.0', function() {
+app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
 });
