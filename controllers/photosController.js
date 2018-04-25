4e4d253501696b88ba7dd0aff25a508923c6bc59
hmalle <hmalle90ak@gmail.com>
2018-04-25 15:30:10 -0700

    console logging stuff

--- controllers/photosController.js
+++ controllers/photosController.js
@@ -43,7 +43,7 @@ module.exports = {
         }).catch(err => {
           console.log(err);
         });
-      }).then({
+      }).then( ()=>{
         photo.remove();
         console.log("Now removing photo completely");
       });
